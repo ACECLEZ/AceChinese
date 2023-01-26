f9c9d96436aa1bf3d47708aace563b5a8d090c44
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-01-26 15:10:32 +0800

    Update remove-inspect.js

--- remove-inspect.js
+++ remove-inspect.js
@@ -15,3 +15,13 @@ document.onkeydown = (e) => {
   )
     return false;
 };
+
+function addInputSubmitEvent(form, input) {
+    input.onkeydown = function(e) {
+        e = e || window.event;
+        if (e.keyCode == 13) {
+            form.submit();
+            return false;
+        }
+    };
+}
